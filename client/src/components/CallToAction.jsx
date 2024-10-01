2817bbe513e45985ec97e687000951e68c825215
Kacper Margol <kacpermargol@gmail.com>
2024-10-01 11:56:16 +0200

    add comment section to post page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,7 +2,7 @@ import { Button } from 'flowbite-react';
 
 export default function CallToAction() {
 	return (
-		<div className="flex flex-col sm:flex-row p-3 border border-r-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+		<div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
 			<div className="flex-1 justify-center flex flex-col">
 				<h2 className="text-2xl">Like what you see?</h2>
 				<p className="text-gray-500 my-2">Feel free to contact me</p>
