008cadfaa0b6f8cf25492ed5f00a3c6a7a977824
Kacper Margol <kacpermargol@gmail.com>
2024-10-08 11:09:55 +0200

    add preinstall

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -15,6 +16,8 @@ mongoose
 	.then(() => console.log('MongoDB is on!'))
 	.catch((err) => console.error(err));
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -24,6 +27,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
