575ab0ee83cb2c1a68842d28d80e7d26d9f1ca33
Kacper Margol <kacpermargol@gmail.com>
2025-01-08 12:41:04 +0100

    Enhance post management: Improve delete and update post logic to check for post existence and user permissions, ensuring only authorized users can modify or delete posts.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -81,13 +81,18 @@ export const getposts = async (req, res, next) => {
 };
 
 export const deletepost = async (req, res, next) => {
-	if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-		return next(
-			errorHandler(403, 'You are not allowed to delete this post'),
-		);
-	}
-
 	try {
+		const post = await Post.findById(req.params.postId);
+		if (!post) {
+			return next(errorHandler(404, 'Post not found'));
+		}
+
+		if (!req.user.isAdmin && post.userId !== req.user.id) {
+			return next(
+				errorHandler(403, 'You are not allowed to delete this post'),
+			);
+		}
+
 		await Post.findByIdAndDelete(req.params.postId);
 		res.status(200).json('The post has been deleted');
 	} catch (error) {
@@ -96,13 +101,22 @@ export const deletepost = async (req, res, next) => {
 };
 
 export const updatepost = async (req, res, next) => {
-	if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-		return next(
-			errorHandler(403, 'You are not allowed to modify this post'),
-		);
-	}
-
 	try {
+		const post = await Post.findById(req.params.postId);
+		if (!post) {
+			return next(errorHandler(404, 'Post not found'));
+		}
+
+		if (!req.user.isAdmin && post.userId !== req.user.id) {
+			return next(
+				errorHandler(403, 'You are not allowed to modify this post'),
+			);
+		}
+
+		if (!req.body.title || !req.body.content) {
+			return next(errorHandler(400, 'Title and content are required'));
+		}
+
 		const updatedPost = await Post.findByIdAndUpdate(
 			req.params.postId,
 			{
